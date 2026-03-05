c48308ea01b945562e1c07d0a876546a67eac7a5
Stephanie Clay <stephclay@users.noreply.github.com>
2026-03-05 10:20:09 -0800

    Revert TripletDTO to being a data class

--- src/main/kotlin/com/wombatsw/raytracing/scene/dto/TripletDTO.kt
+++ src/main/kotlin/com/wombatsw/raytracing/scene/dto/TripletDTO.kt
@@ -4,7 +4,7 @@ import com.wombatsw.raytracing.model.Triplet
 import com.wombatsw.raytracing.scene.ResolutionContext
 import com.wombatsw.raytracing.scene.Resolvable
 
-class TripletDTO(val list: List<Double>) : Resolvable<Triplet> {
+data class TripletDTO(val list: List<Double>) : Resolvable<Triplet> {
     override fun resolve(ctx: ResolutionContext): Triplet =
         Triplet(list[0], list[1], list[2])
 }
